b1a47e6d9e0efc2a3eb9ca927199cb0a774665c9
Aminda Suomalainen <suomalainen+git@mikaela.info>
2022-01-23 13:52:16 +0200

    irc/generate-*: add missing #lappi

--- irc/generate-amode-cmds.bash
+++ irc/generate-amode-cmds.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Looping through all the channels
-for channel in \#helsinki \#keski-suomi \#pirkanmaa \#oulu \#puoluevaltuusto \#puoluetoimisto \#pinu_hallitus \#uusimaa \#varsinais-suomi \#akateemisetpiraatit \#piraattinuoret \#kampanjateltta \#sateenkaaripiraatit \#hame \#vaasa \#hallituskokous \#grafiikka \#toiminta \#piraattivpn \#ppfi \#hepihalko \#aaltopiraatit \#kaakko \#mediaseuranta \#savo-karjala \#messi \#piraatit \#suomiareena \#satakunta \#verkkopalvelut
+for channel in \#helsinki \#keski-suomi \#pirkanmaa \#oulu \#puoluevaltuusto \#puoluetoimisto \#pinu_hallitus \#uusimaa \#varsinais-suomi \#akateemisetpiraatit \#piraattinuoret \#kampanjateltta \#sateenkaaripiraatit \#hame \#vaasa \#hallituskokous \#grafiikka \#toiminta \#piraattivpn \#ppfi \#hepihalko \#aaltopiraatit \#kaakko \#mediaseuranta \#savo-karjala \#messi \#piraatit \#suomiareena \#satakunta \#verkkopalvelut \#lappi
 do
     # Printing the lines to be copy-pasted to IRC
     # Example output: /quote cs amode #channel +o MI1
